9c8cbdce3c439e328127c889a101b8238d767818
ZSP22SCM42Z <zzheng27@hawk.iit.edu>
2022-07-31 08:01:38 -0500

    Added regulate.py

---
+++ data/InputFiles/regulate.py
@@ -0,0 +1,27 @@
+import sys
+
+
+def main():
+    args = sys.argv
+    fileName = args[1]
+    newFileName = "".join(fileName.split(".")[:-1]) + "_regulate.swf"
+    fin = open(args[1], 'r')
+    fout = open(args[2], 'a')
+    lines = fin.readlines()
+    fin.close()
+    ls = ""
+    cnt = 0
+    for line in lines:
+        if line[0] == ';':
+            ls += line
+        else:
+            new_line = ' '.join(line.split())
+            new_line = ' '.join([str(cnt)] + new_line.split(' ')[1:]) + '\n'
+            ls += new_line
+            cnt += 1
+    fout.write(ls)
+    fout.close()
+
+
+if __name__ == "__main__":
+    main()
